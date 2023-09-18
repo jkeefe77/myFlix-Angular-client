376c604809053b15c09295012c38a7c52d46496e
jkeefe77 <jvkeefe@gmail.com>
2023-09-18 14:20:39 -0400

    login update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  showProfileLink: boolean = false;
+  showMoviesLink: boolean = false;
+  showLogoutLink: boolean = false;
+  showBurgerMenu: boolean = true;
+
   // searchTerm: string = '';
   // searchResults: any[] = [];
 
@@ -28,7 +33,19 @@ export class NavbarComponent {
   //     // If the search term is empty, you can handle it here or do nothing
   //   }
   // }
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = event.url;
 
+        // Set the variables based on the current route
+        this.showProfileLink = currentRoute !== '/welcome';
+        this.showMoviesLink = currentRoute !== '/welcome';
+        this.showLogoutLink = currentRoute !== '/welcome';
+        this.showBurgerMenu = currentRoute !== '/welcome';
+      }
+    });
+  }
   logoutUser(): void {
     localStorage.removeItem('user');
     localStorage.removeItem('token');
--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -89,4 +89,16 @@ export class ProfilePageComponent implements OnInit {
       }
     );
   }
+  deleteUser(): void {
+    if (confirm('are you sure?')) {
+      this.router.navigate(['welcome']).then(() => {
+        this.snackBar.open('You have successfully deleted your account', 'OK', {
+          duration: 2000,
+        });
+      });
+      this.fetchApiData.deleteUser().subscribe((result) => {
+        localStorage.clear();
+      });
+    }
+  }
 }
